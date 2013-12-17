80f0e1cb635ba305fff609436e65cc260a9e62f9
Pavel Drobushevich <pavel@targetprocess.com>
2013-12-17 10:47:03 +0300

    Init: app to show all account.

---
+++ Config/Dao/ServerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.IO;
+using Config.Model;
+
+namespace Config.Dao
+{
+	public class ServerRepository
+	{
+		private static readonly string ServersFolder =
+			Path.Combine(Path.GetFullPath(ConfigurationManager.AppSettings["GitFolder"]), "servers");
+
+		public Server GetServer(string id, bool fetch = true)
+		{
+			if (fetch)
+				GitClient.Fetch();
+			var serverFile = Path.Combine(ServersFolder, id + ".json.cfg");
+			return Base.FromJson<Server>(File.ReadAllText(serverFile));
+		}
+	}
+}
---
+++ Web/Controllers/AccountsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Config.Dao;
+using log4net;
+
+namespace Web.Controllers
+{
+	public class AccountsController : BootstrapBaseController
+	{
+		private static readonly ILog Log = LogManager.GetLogger(typeof(AccountsController));
+
+		private readonly AccountRepository _accountRepository = new AccountRepository();
+
+		[BasicAuthentication]
+		public ActionResult Index()
+		{
+			var accounts = _accountRepository.All();
+			return View(accounts);
+		}
+
+		[BasicAuthentication]
+		public ActionResult Tp3(string id)
+		{
+			Information("TP3 acess provided for " + id + ", button will appear in the next few minutes.");
+			return RedirectToAction("Index");
+		}
+	}
+}
